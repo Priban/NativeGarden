ec3cda82eced95b0e951a594fe254193281a5cde
Pribis <lukaspriban01@gmail.com>
2023-02-27 20:08:05 +0100

    plants state reset button, touch swipe recongition

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Canvas, Circle, Paint, vec, LinearGradient } from "@shopify/react-native-skia";
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Canvas, Line, vec, useSpring, useTouchHandler } from "@shopify/react-native-skia";
 import Plant from './garden/Plant';
+import { useState, useEffect } from "react";
+
+const width = 414;
+const height = 800;
 
 export default function App() {
-  const width = 414;
-  const height = 896;
+
+  const [plants, setPlants] = useState([]);
+  const [touch, setTouch] = useState({ start: vec(0, 0), end: vec(0, 0) });
+  const [touching, setTouching] = useState(false);
+  const lineOpacity = useSpring(touching ? 1 : 0);
+
+  const reset = () => {
+    setPlants([]);
+  }
+
+  const addPlant = (x, y) => {
+    setPlants(p => {
+      return [...p, <Plant c={vec(x, y)} a={0} key={p.length} />];
+    });
+  }
+
+  const onCanvasTouch = useTouchHandler({
+    onStart: ({ x, y }) => {
+      setTouch(t => ({ start: vec(x, y), end: vec(x, y) }));
+      setTouching(true);
+    },
+    onActive: ({ x, y }) => {
+      setTouch(t => ({ ...t, end: vec(x, y) }));
+    },
+    onEnd: (event) => {
+      console.log("touch end");
+      console.log(event);
+
+      // distinguish between a tap and a swipe
+      if (Math.abs(touch.start.x - x) < 10 && Math.abs(touch.start.y - y) < 10) {
+        console.log("tapped");
+        addPlant(x, y);
+      } else {
+
+      }
+
+      setTouch(t => {
+        return { ...t, end: vec(x, y) }
+      });
+      setTouching(false);
+    },
+  });
 
   return (
-    <Canvas style={{ width, height, backgroundColor: "brown" }}>
-      <Plant c={vec(200, 700)} a={0} />
-    </Canvas>
+    <>
+      <Canvas style={{ width, height, backgroundColor: "brown" }} onTouch={onCanvasTouch}>
+        {plants}
+        <Line
+          p1={touch.start}
+          p2={touch.end}
+          color={touching ? "lightblue" : "red"}
+          style="stroke"
+          strokeWidth={2}
+          opacity={lineOpacity}
+        />
+      </Canvas>
+      <View style={styles.bottomMenu}>
+        <Button
+          color="white"
+          title="Reset"
+          onPress={reset}
+        />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  bottomMenu: {
     flex: 1,
-  },
+    backgroundColor: "brown",
+    borderTop: "1px solid black",
+  }
 });
--- garden/Plant.js
+++ garden/Plant.js
@@ -1,17 +1,31 @@
 import { Path, Circle, Paint, vec, LinearGradient, Skia, useValue } from "@shopify/react-native-skia";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import Branch from "./Branch";
 import { TreeStructure, Node } from '../utils/TreeStructure';
 import { rotateVector } from "../utils/VectorAlgebra";
 
-const branchSize = 10;
-const dispersion = 180;
+const branchSize = 15;
+const dispersion = 80;
+const maxBranches = 100;
+const maxAngle = 110;
 
-export default function Plant({ c, a }) {
+export default function Plant({ c, a, t }) {
 
   const [plant, setPlant] = useState(new TreeStructure(new Node({ position: c, angle: a, size: branchSize, orientation: 0 })));
 
+   useEffect(() => {
+    const interval = setInterval(() => {
+      grow();
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
   const grow = () => {
+    if (plant.getAllNodes().length > maxBranches) {
+      return;
+    }
+
     setPlant(p => {
       // copy the current plant
       const plantCopy = Object.assign(Object.create(Object.getPrototypeOf(p)), p)
@@ -26,7 +40,7 @@ export default function Plant({ c, a }) {
 
         leaf.children.push(new Node({
           position: vec(position.x + v.x, position.y + v.y),
-          angle: newAngle > 90 ? 90 : newAngle < -90 ? -90 : newAngle,
+          angle: newAngle > maxAngle ? maxAngle : newAngle < -maxAngle ? -maxAngle : newAngle,
           size: size,
           orientation: Math.random() > 0.5 ? 1 : 0
         }));
@@ -35,14 +49,6 @@ export default function Plant({ c, a }) {
       return plantCopy;
     });
   }
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      grow();
-    }, 100);
-
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <>
